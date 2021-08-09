a2c5b9d7513531d48c5a13cbcfc89f542fc0d05d
jsmmmmm <jsmmmmm8@163.com>
2021-08-09 16:12:21 +0800

    1

--- healthcare_information_system/settings.py
+++ healthcare_information_system/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-82r_1)+30h=vbe@3ai(^-agl$67&dn_7+o^k&k00bwrd)vynlg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hcms.xcfans.top']
 
 
 # Application definition
